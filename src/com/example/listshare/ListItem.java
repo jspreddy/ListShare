9df25fb1914cfced8732898cf2449b96d7664a38
richak <richakandlikar@gmail.com>
2013-11-30 18:25:25 -0500

    Created ListItem and ListItemAdapter class

---
+++ src/com/example/listshare/ListItem.java
@@ -0,0 +1,13 @@
+package com.example.listshare;
+
+public class ListItem {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/example/listshare/ListItemAdapter.java
@@ -0,0 +1,16 @@
+package com.example.listshare;
+
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+public class ListItemAdapter extends ArrayAdapter<ListItem>{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
