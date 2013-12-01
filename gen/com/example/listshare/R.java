6994cd27439ee740c08d9d6f8a82120ff5d22e41
richak <richakandlikar@gmail.com>
2013-12-01 02:13:41 -0500

    Added additem activity

--- gen/com/example/listshare/R.java
+++ gen/com/example/listshare/R.java
@@ -37,23 +37,26 @@ public final class R {
         public static final int textView3=0x7f080008;
     }
     public static final class layout {
-        public static final int activity_edit_list_details=0x7f030000;
-        public static final int activity_home=0x7f030001;
-        public static final int activity_main=0x7f030002;
-        public static final int activity_sign_up=0x7f030003;
-        public static final int activity_view_list=0x7f030004;
+        public static final int activity_add_item=0x7f030000;
+        public static final int activity_edit_list_details=0x7f030001;
+        public static final int activity_home=0x7f030002;
+        public static final int activity_main=0x7f030003;
+        public static final int activity_sign_up=0x7f030004;
+        public static final int activity_view_list=0x7f030005;
     }
     public static final class menu {
-        public static final int edit_list_details=0x7f070000;
-        public static final int home=0x7f070001;
-        public static final int main=0x7f070002;
-        public static final int sign_up=0x7f070003;
-        public static final int view_list=0x7f070004;
+        public static final int add_item=0x7f070000;
+        public static final int edit_list_details=0x7f070001;
+        public static final int home=0x7f070002;
+        public static final int main=0x7f070003;
+        public static final int sign_up=0x7f070004;
+        public static final int view_list=0x7f070005;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_add_item=0x7f050007;
         public static final int title_activity_edit_list_details=0x7f050004;
         public static final int title_activity_home=0x7f050003;
         public static final int title_activity_sign_up=0x7f050006;
