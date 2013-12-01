591267956395631947dda10c4bfaba50cfc67e28
richak <richakandlikar@gmail.com>
2013-12-01 18:29:49 -0500

    Created layout for AddItem Activity

--- src/com/example/listshare/AddItemActivity.java
+++ src/com/example/listshare/AddItemActivity.java
@@ -2,14 +2,61 @@ package com.example.listshare;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddItemActivity extends Activity {
 
+	EditText t1,t2,t3;
+	Button b1,b2;
+	String name, quantity, count;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_item);
+		t1 = (EditText) findViewById(R.id.editText1);
+		t2 = (EditText) findViewById(R.id.editText2);
+		t3 = (EditText) findViewById(R.id.editText3);
+		b1 = (Button) findViewById(R.id.button1);
+		b2 = (Button) findViewById(R.id.button2);
+		t1.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				isEmpty(t1);
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub		
+			}
+		});
+		
+		b1.setOnClickListener(new OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				executeQuery();
+			}
+		});
+		
+		b2.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
 	}
 
 	@Override
@@ -17,6 +64,21 @@ public class AddItemActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.add_item, menu);
 		return true;
+	}
+
+	public boolean isEmpty(EditText et) {
+		String text = et.getText().toString();
+		if (text.isEmpty()) {
+			et.setError("This cannot be empty.");
+			return true;
+		}
+		et.setError(null);
+		return false;
+	}
+	
+	private void executeQuery() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
