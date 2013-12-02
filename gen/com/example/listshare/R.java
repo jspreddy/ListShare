895067ea0f7f90a8ec1caafbec24710c93ed4115
Sai Phaninder Reddy Jonnala <jspreddy@yahoo.com>
2013-12-01 21:48:37 -0500

    -ignore the gen files

--- gen/com/example/listshare/R.java
+++ gen/com/example/listshare/R.java
@@ -19,22 +19,27 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
+        public static final int item_horz_margin=0x7f040002;
+        public static final int item_vert_margin=0x7f040003;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f080002;
-        public static final int action_settings=0x7f08000a;
-        public static final int button1=0x7f080001;
-        public static final int button2=0x7f080007;
+        public static final int action_settings=0x7f08000d;
+        public static final int btnListAdd=0x7f080001;
+        public static final int button1=0x7f080007;
+        public static final int button2=0x7f080008;
         public static final int editText1=0x7f080004;
         public static final int editText2=0x7f080006;
-        public static final int editText3=0x7f080009;
-        public static final int listView1=0x7f080000;
+        public static final int editText3=0x7f08000a;
+        public static final int lvMainList=0x7f080000;
         public static final int textView1=0x7f080003;
         public static final int textView2=0x7f080005;
-        public static final int textView3=0x7f080008;
+        public static final int textView3=0x7f080009;
+        public static final int tvMainOwner=0x7f08000c;
+        public static final int tvMainTitle=0x7f08000b;
     }
     public static final class layout {
         public static final int activity_edit_list_details=0x7f030000;
@@ -42,7 +47,7 @@ public final class R {
         public static final int activity_main=0x7f030002;
         public static final int activity_sign_up=0x7f030003;
         public static final int activity_view_list=0x7f030004;
-        public static final int main_list=0x7f030005;
+        public static final int main_list_item=0x7f030005;
     }
     public static final class menu {
         public static final int edit_list_details=0x7f070000;
@@ -52,8 +57,11 @@ public final class R {
         public static final int view_list=0x7f070004;
     }
     public static final class string {
+        public static final int Owner=0x7f050009;
+        public static final int Title=0x7f050008;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int btnListAdd=0x7f050007;
         public static final int hello_world=0x7f050002;
         public static final int title_activity_edit_list_details=0x7f050004;
         public static final int title_activity_home=0x7f050003;
