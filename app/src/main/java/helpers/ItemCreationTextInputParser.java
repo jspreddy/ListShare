17126a9ac952f833825ea93d08336442e3a0305b
Sai Phaninder Reddy Jonnala <jspreddy@yahoo.com>
2015-01-18 22:32:42 -0500

    parsing out count from input string done.

--- app/src/main/java/helpers/ItemCreationTextInputParser.java
+++ app/src/main/java/helpers/ItemCreationTextInputParser.java
@@ -1,5 +1,8 @@
 package helpers;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by Sai on 1/18/2015.
  */
@@ -8,12 +11,16 @@ package helpers;
 public class ItemCreationTextInputParser {
     enum InputCredibility {FULL, MEDIUM, LOW}
 
-    private int count;
-    private String itemName;
-    private int qty;
-    private String unit;
+    private int count = -1;
+    private String itemName = "";
+    private int qty = -1;
+    private String unit = "";
     private InputCredibility crediblity;
+    private List<String> input;
 
+    private int DEFAULT_COUNT = 1;
+    private int DEFAULT_QTY = 1;
+    private String DEFAULT_UNIT = "";
 
     ItemCreationTextInputParser(String input) {
         this.parseInputString(input);
@@ -28,8 +35,38 @@ public class ItemCreationTextInputParser {
      * "item name 4"
      * "item name"
      */
-    private void parseInputString(String input) {
-        String[] array = input.split("\\s", -1);
+    private void parseInputString(String str) {
+        String[] array = str.split("\\s", -1);
+        if(array.length<=0){
+            crediblity = InputCredibility.LOW;
+        }
+        input = Arrays.asList(array);
+        this.count = this.parseOutCountFromString();
+        this.qty  = this.parseOutQtyFromString();
+    }
+
+    private int parseOutCountFromString(){
+        while(input.size()>0){
+            String currentString = input.get(0);
+            if(currentString.isEmpty()) {
+                input.remove(0);
+                continue;
+            }
+
+            if(currentString.matches("\\d+")){
+                input.remove(0);
+                return Integer.parseInt(currentString);
+            }
+            else{
+                break;
+            }
+        }
+        return DEFAULT_COUNT;
+    }
+
+    private int parseOutQtyFromString(){
+
+        return DEFAULT_QTY;
     }
 
     public int getCount() {
